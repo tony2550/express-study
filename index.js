7c96651373df362f230ec39312df122d39031aec
김도현 <mookieum1991@gmail.com>
2022-05-19 10:05:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3030);
+
+app.get('/', (req, res) => {
+  res.send('Local Express Server');
+});
+
+app.listen(app.get('port'), '0.0.0.0', () => {
+  console.log(`on port ${app.get('port')}`);
+});
